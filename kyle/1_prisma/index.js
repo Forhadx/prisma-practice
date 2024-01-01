43f5bee9d511a9af55e2edcbf6e92401f6bd4aca
forhadx <forhadsh1@gmail.com>
2024-01-01 22:16:59 +0600

    init prisma

--- kyle/1_prisma/index.js
+++ kyle/1_prisma/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+app.use(express.json());
 
 app.listen(5001, () => {
   console.log("server listening on port: 5001");
